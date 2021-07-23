f6203556e8597d450aa9271751ac5b6d566314e2
unknown <lintang@cpssoft.com>
2021-07-23 20:57:32 +0700

    Initial commit

---
+++ lib/model/score_model.dart
@@ -0,0 +1,59 @@
+class ScoreMatch {
+  Fixture fixture;
+  Team home;
+  Team away;
+  Goal goal;
+
+  ScoreMatch(this.fixture, this.home, this.away, this.goal);
+  factory ScoreMatch.fromJson(Map<String, dynamic> json) {
+    return ScoreMatch(
+        Fixture.fromJson(json['fixture']),
+        Team.fromJson(json['teams']['home']),
+        Team.fromJson(json['teams']['home']),
+        Goal.fromJson(json['goal']));
+  }
+}
+
+class Fixture {
+  int id;
+  String date;
+  Status status;
+  Fixture(this.id, this.date, this.status);
+
+  factory Fixture.fromJson(Map<String, dynamic> json) {
+    return Fixture(json['id'], json['date'], Status.fromJson(json['status']));
+  }
+}
+
+class Status {
+  int elapsedTime;
+  String long;
+  Status(this.elapsedTime, this.long);
+
+  factory Status.fromJson(Map<String, dynamic> json) {
+    return Status(json['elapsed'], json['long']);
+  }
+}
+
+class Team {
+  int id;
+  String name;
+  String logo;
+  bool winner;
+
+  Team(this.id, this.name, this.logo, this.winner);
+
+  factory Team.fromJson(Map<String, dynamic> json) {
+    return Team(json["id"], json["name"], json["logo"], json["winner"]);
+  }
+}
+
+class Goal {
+  int home;
+  int away;
+  Goal(this.home, this.away);
+
+  factory Goal.fromJson(Map<String, dynamic> json) {
+    return Goal(json['home'], json['away']);
+  }
+}
---
+++ lib/network/football_api.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+
+import 'package:football_app/model/score_model.dart';
+import 'package:http/http.dart';
+
+class FootballApi {
+  final String apiUrl =
+      "https://api-football-v1.p.rapidapi.com/v3/fixtures?league=39&season=2020";
+
+  static const headers = {
+    'x-rapidapi-key': '8ed6c682c7msh24d7e011432a221p10f4f5jsn893de4d8ea76',
+    'x-rapidapi-host': 'api-football-v1.p.rapidapi.com'
+  };
+
+  Future<List<ScoreMatch>> getAllScore() async {
+    Response res = await get(Uri.parse(apiUrl), headers: headers);
+    var body;
+
+    if (res.statusCode == 200) {
+      body = jsonDecode(res.body);
+      List<dynamic> response = body["response"];
+      List<ScoreMatch> scoreMatches =
+          response.map((dynamic json) => ScoreMatch.fromJson(json)).toList();
+      return scoreMatches;
+    }
+    return [];
+  }
+}
